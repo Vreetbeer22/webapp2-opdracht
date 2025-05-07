278a11ae7eedd86a3e6c946478aaad6f78bf71f6
cas <caswenting@gmail.com>
2025-05-07 08:47:01 +0200

    bestanden aangemaakt om in te gaan werken

---
+++ html/overons.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav>
+
+    </nav>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
