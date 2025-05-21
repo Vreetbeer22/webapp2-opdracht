e5ac722697deac3a3e49d2694fe98fc275b2ef42
cas <caswenting@gmail.com>
2025-05-21 09:50:53 +0200

    bezig met zoekbalk
    maken van zoekbalk in de home pagina.
    moet nu zorgen dat de tekst op het plaatje kan komen te staan

--- html/index.php
+++ html/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
-    <title>Document</title>
+    <title>Home pagina</title>
 </head>
 
 <body>
@@ -19,6 +19,10 @@
     <main>
         <div class="home_zoekblok">
             <img src="images/achtergrond_zoekblok.jpg" class="achtergrond_zoekblok" alt="plaatje met pad richting zon">
+            <div class="tekst_zoekblok">
+                <h2>Waar begint jouw reis?</h2>
+            </div>
+            <div class="zoekbalk_zoekblok"></div>
         </div>
     </main>
     <footer>
