08fe34218acbe3e79aaa30738be3d24936b81722
cas <caswenting@gmail.com>
2025-05-22 11:22:22 +0200

    verder gewerkt aan zoekblok
    tekst en zoekbalk staan nu boven op de achtergrond

--- html/index.php
+++ html/index.php
@@ -5,24 +5,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Mouse+Memoirs&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <title>Home pagina</title>
 </head>
 
 <body>
     <nav>
         <div class="header">
-            <div class="header_content">
+            <div class="header-content">
                 
             </div>
         </div>     
     </nav>
     <main>
-        <div class="home_zoekblok">
-            <img src="images/achtergrond_zoekblok.jpg" class="achtergrond_zoekblok" alt="plaatje met pad richting zon">
-            <div class="tekst_zoekblok">
+        <div class="home-zoekblok">
+            <img src="images/achtergrond-zoekblok.jpg" class="achtergrond-zoekblok" alt="plaatje met pad richting zon">
+            <div class="tekst-zoekblok">
                 <h2>Waar begint jouw reis?</h2>
             </div>
-            <div class="zoekbalk_zoekblok"></div>
+            <div class="zoekbalk-zoekblok">
+                <div class="zoekknop-zoekblok">
+                    
+                </div>
+            </div>
         </div>
     </main>
     <footer>
