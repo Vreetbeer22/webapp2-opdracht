8df7eedd42ffb4fbd97920b500853f965f81e65c
Niek809 <niekdeheij23@gmail.com>
2025-05-26 08:39:17 +0200

    header html
    ik heb de header gemaakt voor onze reispagina

--- html/index.php
+++ html/index.php
@@ -6,19 +6,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
     <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Mouse+Memoirs&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <nav>
         <div class="header">
-            <div class="header_content">
-                
+            <div class="header-content">
+                <img class="logo-zerfreis-header" src="images/logo-zwerfreis.png" alt="Zwerfreis logo">
+                <h1>werfreis</h1>
+            </div>
+            <div class="header-content-text">
+                <div class="header-content-links-inlog">
+                    <a href="login.php">Inloggen</a>
+                </div>
+                <div class="header-content-links">
+                    <a href="index.php">Home</a>
+                    <a href="informatie.php">Reizen</a>
+                    <a href="overons.php">Over ons</a>
+                    <a href="contact.php">Contact</a>
+                </div>
             </div>
         </div>     
     </nav>
+
     <main>
-        <div class="home_zoekblok">
-            <img src="images/achtergrond_zoekblok.jpg" class="achtergrond_zoekblok" alt="plaatje met pad richting zon">
+        <div class="home-zoekblok">
+            <img src="images/achtergrond_zoekblok.jpg" class="achtergrond-zoekblok" alt="plaatje met pad richting zon">
         </div>
     </main>
     <footer>
